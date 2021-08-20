572e4a6b4631bfcc73a20eb6ce671865f41cd7ec
Blessing Asuelimen <asuelimenblessing630@gmail.com>
2021-08-20 14:50:42 -0700

    my first push of the e-commerce project from group Anjela

---
+++ models/Product.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, price, product_id, product_name, product_description, product_category):
+        self.price = price
+        self.product_id = product_id
+        self.product_name = product_name
+        self.product_description = product_description
+        self.product_category = product_category
