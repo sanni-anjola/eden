5e7ea02fe00ab6f78fa472eb9a9c2bcbc87122c7
Nwole Chidiebere <nwolechidiebere2017@gmail.com>
2021-08-20 14:51:37 +0100

    my account first Pust

---
+++ models/Customer.py
@@ -0,0 +1,3 @@
+class Customer:
+    def __init__(self,fullName,age,email,address,phone,password):
+        super(self).__init__(fullName,age,email,address,phone,password)
--- models/User.py
+++ models/User.py
@@ -1,2 +1,7 @@
 class user:
-    pass
+    def __int__(self, name,emaill,address,password,phone):
+        self.fullName = name;
+        self .email = emaill
+        self.address = address
+        self.phone = phone
+        self.password = password
